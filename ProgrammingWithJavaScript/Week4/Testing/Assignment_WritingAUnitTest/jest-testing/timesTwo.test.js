64522678e4a62136b0f085f566f3bfad4c982bdc
dkbozkurt <dkaanbozkurt@gmail.com>
2023-10-13 22:37:30 +0200

    Assignment_WritingAUnitTest

---
+++ ProgrammingWithJavaScript/Week4/Testing/Assignment_WritingAUnitTest/jest-testing/timesTwo.test.js
@@ -0,0 +1,7 @@
+const { default: TestRunner } = require("jest-runner");
+const timesTwo = require('./timesTwo');
+
+// Write the first test
+test('returns the number times 2', () => {
+    expect(timesTwo(10)).toBe(20);
+});
