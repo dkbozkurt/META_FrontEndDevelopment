fa60ad63337470162a29487a44b178e4755839e9
dkbozkurt <dkaanbozkurt@gmail.com>
2023-10-13 18:11:29 +0200

    JavaScriptModules

---
+++ ProgrammingWithJavaScript/Week3/JSInBrowser/JavaScriptModules/greeting.js
@@ -0,0 +1,15 @@
+export const informalGreeting = (person) =>
+{
+    console.log(`Hello, ${person}`);
+}
+
+export const formalGreeting = (person) =>
+{
+    console.log(`Good day, ${person}`);
+}
+
+const greeting = () => {
+    console.log(`Howdy!`);
+}
+
+export default greeting;
