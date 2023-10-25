ec69f215d8ba01fc5c046c6e17095e55ca675418
dkbozkurt <dkaanbozkurt@gmail.com>
2023-10-25 23:54:14 +0200

    Lab_Passingprops

---
+++ ReactBasics/Week1/ComponentUseAndStyling/Lab_Passingprops/src/App.js
@@ -0,0 +1,11 @@
+import Heading from "./components/Heading.js";
+
+function App(){
+    return (
+        <div className ="App">
+        <Heading firstname="Bob" />
+        </div>
+    );
+}
+
+export default App;
