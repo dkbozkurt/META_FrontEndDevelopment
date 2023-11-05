938d434d19f756c5c11c9c3778f1d087ab559b2c
dkbozkurt <dkaanbozkurt@gmail.com>
2023-11-05 23:04:53 +0100

    WhatIsContext

---
+++ AdvancedReact/Week1/ReactContext/WhatIsContext/UserContext.js
@@ -0,0 +1,14 @@
+import { createContext, useContext, useState } from "react";
+
+const UserContext = createContext(undefined);
+
+export const UserProvider = ({children}) => {
+    const [user] = useState({
+        name: "John",
+        email: "john@example.com",
+        dob:"01/01/2000",
+    });
+    return <UserContext.Provider value = {{user}} ></UserContext.Provider>;
+};
+
+export const useUser = () => useContext(UserContext);
