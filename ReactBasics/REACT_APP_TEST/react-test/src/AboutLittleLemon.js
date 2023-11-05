71145b9a863d43480815f12b2f2f6ce94ce55a69
dkbozkurt <dkaanbozkurt@gmail.com>
2023-11-05 23:52:25 +0100

    Removed React app test

--- ReactBasics/REACT_APP_TEST/react-test/src/AboutLittleLemon.js
+++
@@ -1,7 +0,0 @@
-function AboutLittleLemon() {
-    return (
-        <h1>About LittleLemon</h1>
-    )
-}
-
-export default AboutLittleLemon
--- ReactBasics/REACT_APP_TEST/react-test/src/Contact.js
+++
@@ -1,9 +0,0 @@
-function Contact(){
-    return(
-        <h1>
-            Contact Little Lemon on this page.
-        </h1>
-    );
-}
-
-export default Contact;
